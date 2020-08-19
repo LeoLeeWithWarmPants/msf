f4f31557efc47233a5d71f69594c422352907926
LeoLee <512240816@qq.com>
2020-08-19 17:54:21 +0800

    Hystrix监控数据聚合(集群方式)

--- msf-hystrix-dashboard/src/main/java/com/leolee/msf/MsfHystrixDashboardApplication.java
+++ msf-hystrix-dashboard/src/main/java/com/leolee/msf/MsfHystrixDashboardApplication.java
@@ -1,9 +1,11 @@
 package com.leolee.msf;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.boot.autoconfigure.SpringBootApplication;import org.springframework.cloud.client.discovery.EnableDiscoveryClient;import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
-@EnableHystrixDashboard
+@EnableTurbine // 开启turbine监控数据聚合
+@EnableHystrixDashboard // 开启hystrix仪表盘
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MsfHystrixDashboardApplication {
 
