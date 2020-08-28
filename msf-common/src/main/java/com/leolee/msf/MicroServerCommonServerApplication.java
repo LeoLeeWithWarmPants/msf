120398e359ac7a6595397e5f7116643551eeda36
LeoLee <512240816@qq.com>
2020-08-28 13:44:50 +0800

    屏蔽common模块的springboot启动

--- msf-common/src/main/java/com/leolee/msf/MicroServerCommonServerApplication.java
+++ msf-common/src/main/java/com/leolee/msf/MicroServerCommonServerApplication.java
@@ -3,7 +3,7 @@ package com.leolee.msf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class MicroServerCommonServerApplication {
 
     public static void main(String[] args) {
